acf2bc274d5265564eaeb1a5b5708a2c5398ba8c
Anton Elshin <anton.elshin@mail.ru>
2021-03-09 13:59:39 +0300

    7. Разработка стартового приложения Angular
    Развернуть приложение с помощью Angular CLI. Удалить шаблон сгенерированный Angular CLI, добавить или изменить переменные класса, вывести их в шаблон используя интерполяцию {{}}.
    
    Добавить базовую верстку и стили будущего приложения.

--- AngTask07/src/app/app.module.ts
+++ AngTask07/src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import ruLocale from '@angular/common/locales/ru';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReferencesComponent } from './pages/references/references.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferencesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
