2322c2f6cfe2677131ad7cd9d30153c3f0181e98
Anton Elshin <anton.elshin@mail.ru>
2021-03-14 02:39:37 +0300

    14. Формы и валидация
    Добавить в приложение форму логина из заданий 3-4. Используя механизм реактивных форм, сделать рефакторинг формы логина, реализовать валидацию.

--- AngTask14/src/app/app.module.ts
+++ AngTask14/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReferencesComponent } from './pages/references/references.component';
+import { ReferenceCreateComponent } from './pages/reference-create/reference-create.component';
+import { ReferenceEditComponent } from './pages/reference-edit/reference-edit.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferencesComponent,
+    ReferenceCreateComponent,
+    ReferenceEditComponent,
+    LoginPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngTask14/src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  form!: FormGroup;
+  loadingExecutingFlag = false;
+  disableFlag = false;
+
+  constructor(private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function(){
+      return false;
+    };
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      login: new FormControl(null, [
+        Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+
+  submit(): void {
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loadingExecutingFlag = true;
+    this.disableFlag = true;
+
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.loadingExecutingFlag = false;
+      this.disableFlag = false;
+
+      if (this.form.controls.login.value === 'dsso' && this.form.controls.password.value === '12345678') {
+        this.form.controls.login.setValue(null);
+        this.form.controls.password.setValue(null);
+        this.router.navigate(['/references']);
+      }
+      else {
+        this.form.controls.login.setValue(null);
+        this.form.controls.password.setValue(null);
+        this.reloadCurrentRoute();
+      }
+
+    }, 2000);
+  }
+
+  reloadCurrentRoute(): void {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+      this.router.navigate([currentUrl]);
+    });
+  }
+}
